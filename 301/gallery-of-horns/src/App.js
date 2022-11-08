91514726140cc0c5c216950d128b1ce3521e6efd
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-11-08 18:27:50 -0500

    fixes

--- 301/gallery-of-horns/src/App.js
+++ 301/gallery-of-horns/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import HornedBeast from "./HornedBeast";
 
 
 class App extends React.Component {
@@ -10,23 +9,7 @@ class App extends React.Component {
     return (
       <>
         <Header />
-        <Main>
-          <HornedBeast
-            title="Narwhal"
-            imageURL="https://img.ccnull.de/1020000/preview/1024404_8ee66cfa1955a96d649873f4f32c99a2.jpg"
-            description=""
-          />
-          <HornedBeast
-            title="Rhino"
-            imageURL=""
-            description=""
-          />
-          <HornedBeast
-            title="Unicorn"
-            imageURL=""
-            description=""
-          />
-        </Main>
+        <Main/>
         <Footer />
       </>)
   }
