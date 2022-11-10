1e294cb532b3bfc2a857643f7ff56997ce11b370
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-11-10 01:53:45 -0500

    progress

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    };
+  }
   render() {
     return (
       <>
         <h2>{this.props.title}</h2>
+        <p>{this.state.likes} likes</p>
         <img 
-          src={this.props.imgageURL} 
+          src={this.props.image_url} 
           alt={this.props.description} 
           title={this.props.title}
         />
--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,26 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import data from './data.json';
 
 class Main extends React.Component {
-
   render() {
-    
-    let url1 ='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-    
-    let url2 ='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
 
-    return (
-      <>
-        <HornedBeast 
-          imageURL={url1}
-          title="UniWhal"
-          description="A unicorn and a narwhal nuzzling their horns"
-        />
-        <HornedBeast
-          imageURL={url2}
-          title="Rhino"
-          description="Parent rhino with two babies"
-        />
-     </>
-    );
+
+    let beastArr = [];
+    //console.log(data);
+    return data.map((beast, idx) => {
+      return beastArr.push(<HornedBeast
+        _id={beast._id}
+        image_url={beast.image_url}
+        title={beast.title}
+        description={beast.description}
+        keyword={beast.keyword}
+        horns={beast.horns}
+        key={idx}
+      />
+      ); 
+    });
   }
 }
 
-export default Main;
+        export default Main;
